b7adf7eb2899c2239f3e0df6f066f7212885e4e7
dk-denno <dennisveer27@gmail.com>
2018-07-05 10:06:10 +0300

    finals

--- calculator.js
+++ calculator.js
@@ -1,5 +1,11 @@
-var add=function(number1,number2){
-  return number1+number2;
+$(document).ready(function(){
+
+var add=function(number1, number2){
+  return number1 + number2;
+};
+  var number1=parseInt(prompt("enter a number:"));
+  var number2=parseInt(prompt("enter another number:"));
+  alert(add(number1, number2));
 };
 var minus=function(number1,number2){
   return number1-number2;
@@ -9,4 +15,15 @@ var divide=function(number1,number2){
 };
 var multiply=function(number1,number2){
   return number1*number2;
+};
+  $("button#green").click(function() {
+  $("body").addClass("green-background");
+});
+  $("button#yellow").click(function() {
+  $("body").addClass("yellow-background");
+});
+ $("button#red").click(function() {
+$("body").addClass("red-background");
+});
+});
 };
